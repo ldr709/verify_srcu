37a2fa035e8485211e96cf8b43b2665c9471d5aa
Lance Roy <ldr709@gmail.com>
2016-07-02 10:35:14 -0700

    Don't create an empty errno.h

--- create_empty_includes.sh
+++ create_empty_includes.sh
@@ -13,3 +13,6 @@ mkdir empty_includes_arch/
 # Just use x86's include tree. It shouldn't matter since they are empty files.
 ( cd linux_source/arch/x86/include/ && find . -type d '(' '!' -path '.' ')' -print0 ) | xargs -0 -I '{}' mkdir 'empty_includes_arch/{}'
 ( cd linux_source/arch/x86/include/ && find . -name "*.h" -print0 ) | xargs -0 -I '{}' touch 'empty_includes_arch/{}'
+
+#Use the system errno.h
+rm empty_includes/linux/errno.h
