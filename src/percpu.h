1e8d91bcf10ccd39bfef73175e2b058468a4d2d6
Lance Roy <ldr709@gmail.com>
2016-07-15 16:07:55 -0700

    Fix per-cpu data and add a test case.

--- src/percpu.h
+++ src/percpu.h
@@ -3,8 +3,12 @@
 
 #include <stddef.h>
 #include "bug_on.h"
+#include "preempt.h"
 
 #define __percpu
+
+/* Maximum size of any percpu data. */
+#define PERCPU_OFFSET (4 * sizeof(long))
 
 /* Ignore alignment, as CBMC doesn't care about false sharing. */
 #define alloc_percpu(type) __alloc_percpu(sizeof(type), 1);
@@ -20,7 +24,8 @@ static inline void free_percpu(void *ptr)
 	BUG();
 }
 
-#define per_cpu_ptr(ptr, cpu) (&(ptr)[cpu])
+#define per_cpu_ptr(ptr, cpu) \
+	((typeof(ptr)) ((char*) (ptr) + PERCPU_OFFSET * cpu))
 
 #define __this_cpu_inc(pcp) __this_cpu_add(pcp, 1)
 #define __this_cpu_dec(pcp) __this_cpu_dec(pcp, 1)
@@ -30,20 +35,35 @@ static inline void free_percpu(void *ptr)
 #define this_cpu_dec(pcp) this_cpu_sub(pcp, 1)
 #define this_cpu_sub(pcp, n) this_cpu_add(pcp, -(typeof(pcp)) (n))
 
+/*
+ * For some reason CBMC needs an atomic operation even though this is percpu
+ * data.
+ */
 #define __this_cpu_add(pcp, n) \
 	do { \
 		BUG_ON(preemptible()); \
-		*per_cpu_ptr(&(pcp), thread_cpu_id) += (n); \
+		(void) __sync_add_and_fetch( \
+			per_cpu_ptr(&(pcp), thread_cpu_id), \
+			(typeof(pcp)) (n)); \
 	} while (0)
 
 #define this_cpu_add(pcp, n) \
 	do { \
 		int this_cpu_add_impl_cpu = get_cpu(); \
-		*per_cpu_ptr(&(pcp), this_cpu_add_impl_cpu) += (n); \
+		(void) __sync_add_and_fetch( \
+			per_cpu_ptr(&(pcp), this_cpu_add_impl_cpu), \
+			(typeof(pcp)) (n)); \
 		put_cpu(); \
 	} while (0)
 
-#define DEFINE_PER_CPU(type, name) typeof(type) name[NR_CPUS]
+/*
+ * This will cause a compiler warning because of the cast from char[][] to
+ * type*. This will cause a compile time error if type is too big.
+ */
+#define DEFINE_PER_CPU(type, name) \
+	char name[NR_CPUS][PERCPU_OFFSET]; \
+	typedef char percpu_too_big_##name \
+		[sizeof(type) > PERCPU_OFFSET ? -1 : 1]
 
 #define for_each_possible_cpu(cpu) \
 	for ((cpu) = 0; (cpu) < NR_CPUS; ++(cpu))
---
+++ tests/percpu/test.c
@@ -0,0 +1,41 @@
+#include <src/combined_source.c>
+
+DEFINE_PER_CPU(struct srcu_struct_array, per_cpu_data);
+
+struct srcu_struct_array __percpu *per_cpu_ref = &per_cpu_data;
+
+void *thread(void *arg)
+{
+	this_cpu_inc(per_cpu_ref->c[0]);
+	smp_mb();
+	this_cpu_inc(per_cpu_ref->seq[0]);
+
+	return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+	pthread_t t0;
+	pthread_t t1;
+
+	if (pthread_create(&t0, NULL, thread, NULL))
+		abort();
+	if (pthread_create(&t1, NULL, thread, NULL))
+		abort();
+	if (pthread_join(t0, NULL))
+		abort();
+	if (pthread_join(t1, NULL))
+		abort();
+
+	unsigned long sum_c = 0;
+	sum_c += READ_ONCE(per_cpu_ptr(per_cpu_ref, 0)->c[0]);
+	sum_c += READ_ONCE(per_cpu_ptr(per_cpu_ref, 1)->c[0]);
+
+	unsigned long sum_seq = 0;
+	sum_seq += READ_ONCE(per_cpu_ptr(per_cpu_ref, 0)->seq[0]);
+	sum_seq += READ_ONCE(per_cpu_ptr(per_cpu_ref, 1)->seq[0]);
+
+	assert(sum_c == 2 && sum_seq == 2);
+
+	return 0;
+}
