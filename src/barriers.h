b0414ec1f596472beebdcc53d24d5350d95630d8
Lance Roy <ldr709@gmail.com>
2016-07-31 17:07:27 -0700

    Work arround CBMC bug in __sync_synchronize.
    Also add macro to disable memory barriers.

--- src/barriers.h
+++ src/barriers.h
@@ -2,7 +2,22 @@
 #define BARRIERS_H
 
 #define barrier() __asm__ __volatile__("": : :"memory")
+
+#ifndef NO_SMP_MB
+
+#ifdef RUN
 #define smp_mb() __sync_synchronize()
+#else
+/* Copied from CBMC's implementation of __sync_synchronize(), which seems to be
+   disabled by default. */
+#define smp_mb() __CPROVER_fence("WWfence", "RRfence", "RWfence", "WRfence", \
+				 "WWcumul", "RRcumul", "RWcumul", "WRcumul")
+#endif
+
+#else
+#define smp_mb() do {} while (0)
+
+#endif
 
 #define READ_ONCE(x) (*(volatile typeof(x) *) &(x))
 #define WRITE_ONCE(x, val) (*(volatile typeof(x) *) &(x) = (val))
